1a46c86d59a4387484debee6baf9b256af9d35d4
deivisjl <deivisjl@gmail.com>
2021-04-23 10:21:15 -0600

    Pagina 404

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -50,6 +50,10 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if($exception->getStatusCode() == 404)
+        {
+            return response()->view('errors.404');
+        }
         return parent::render($request, $exception);
     }
 }
